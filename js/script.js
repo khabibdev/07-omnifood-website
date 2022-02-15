1002572a7fa2704c2085621b717626bd4fe7d8b3
Khabib <khabibdev@gmail.com>
2022-02-15 16:08:39 -0500

    finish mobile-navigation-work

--- js/script.js
+++ js/script.js
@@ -12,10 +12,20 @@ window.addEventListener("DOMContentLoaded", (event) => {
     //     h1.style.backgroundColor = "red";
     //     h1.style.padding = "5rem";
     // });
+
+    // Set current year
     const yearEl = document.querySelector(".year");
     const currentYear = new Date().getFullYear();
     console.log(currentYear);
     yearEl.textContent = currentYear;
+
+    // Make mobile navigation work
+    const btnNavEl = document.querySelector(".btn-mobile-nav");
+    const headerEl = document.querySelector(".header");
+
+    btnNavEl.addEventListener("click", function () {
+        headerEl.classList.toggle("nav-open");
+    });
 });
 
 // // Fixing flexbox gap property missing in some Safari versions
