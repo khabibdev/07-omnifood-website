8e7cf7ffa711c75c0b919e6de433a1a11807f677
Khabib <khabibdev@gmail.com>
2022-02-16 09:48:15 -0500

    finish smooth-scroling

--- js/script.js
+++ js/script.js
@@ -26,6 +26,28 @@ window.addEventListener("DOMContentLoaded", (event) => {
     btnNavEl.addEventListener("click", function () {
         headerEl.classList.toggle("nav-open");
     });
+
+    // Smooth scrolling animation
+    const alllinks = document.querySelectorAll("a:link");
+
+    alllinks.forEach(function (link) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const href = link.getAttribute("href");
+
+            // scroll back to top
+            if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+            // Scroll to other links
+            if (href !== "#" && href.startsWith("#")) {
+                const sectionEl = document.querySelector(href);
+                sectionEl.scrollIntoView({ behavior: "smooth" });
+            }
+
+            // Close mobile navigation
+            if (link.classList.contains("main-nav-link")) headerEl.classList.toggle("nav-open");
+        });
+    });
 });
 
 // // Fixing flexbox gap property missing in some Safari versions
